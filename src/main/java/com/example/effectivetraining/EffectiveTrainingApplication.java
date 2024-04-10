b93f1452cabae7429477b986c64934016b7a5842
someliukh <stepan.omeliukh.work@gmail.com>
2024-04-10 22:20:38 +0300

    Init commit

---
+++ src/main/java/com/example/effectivetraining/EffectiveTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.effectivetraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectiveTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/effectivetraining/EffectiveTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.effectivetraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
